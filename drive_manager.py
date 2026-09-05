f1dcc939f0aa3996c7f7434ccab0d8ba0e07abd1
pbvenkov <pbvenkov@gmail.com>
2026-09-05 10:55:37 +0300

    Refactor DriveManager initialization and comments
    Removed unnecessary comments and updated the class initialization.

--- drive_manager.py
+++ drive_manager.py
@@ -3,19 +3,14 @@ import requests
 
 class DriveManager:
     def __init__(self, credentials=None):
-        # Пароли от сервисного аккаунта нам больше не нужны, мы их игнорируем!
-        
-        # 1. ВСТАВЬТЕ СЮДА ССЫЛКУ ИЗ GOOGLE APPS SCRIPT (которую вы скопировали на Шаге 2)
         self.web_app_url = "https://script.google.com/macros/s/AKfycbxqgqhSOz6S-qepOiJ0t5r3UizTbuiqPcg4GOqqcOP6cXZ04Ia1PiJtvzV-3Kpm_CxqTg/exec"
         
-        # 2. ВСТАВЬТЕ СЮДА ВАШИ ID ПАПОК
         self.pdf_root_id = "15kzKEaS76HAhx22FR-BTvifbaecH_wx8"
         self.json_root_id = "1efm3iHSVvUPp50in3tfOGxd0xOACio2E"
         self.letters_root_id = "10hP476EXoiPCkRfE9nqc1ZyyTBNvPKR6"
 
     def upload_file(self, filename, content, mime_type, root_folder_id):
         try:
-            # Конвертируем контент в base64 для надежной передачи через интернет
             if isinstance(content, str):
                 content = content.encode('utf-8')
             
@@ -28,7 +23,6 @@ class DriveManager:
                 "mimeType": mime_type
             }
             
-            # Отправляем файл вашему скрипту-приемнику
             response = requests.post(self.web_app_url, json=payload, timeout=60)
             result = response.json()
             
